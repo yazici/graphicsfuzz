13ff3407397e6badff45b7379d2d14363dc869e1
Hugues Evrard <hevrard@google.com>
2019-02-11 19:12:14 +0000

    Fix CI: cmake module path

--- build/travis/build-vulkan-worker-linux.sh
+++ build/travis/build-vulkan-worker-linux.sh
@@ -27,7 +27,7 @@ mkdir -p vulkan-worker/build
 
 pushd vulkan-worker/build
 rm -rf ./*
-cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MODULE_PATH="${HOME}/local/glfw"
+cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MODULE_PATH="${HOME}/local/glfw/lib/cmake/glfw3/"
 cmake --build . --config Debug -- -j$(nproc)
 popd
 
