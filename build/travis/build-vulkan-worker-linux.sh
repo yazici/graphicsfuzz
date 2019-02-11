9bc4bed86c80f2ece0b1c941c525af1a6abc10e5
Hugues Evrard <hugues.evrard@graphicsfuzz.com>
2019-02-11 22:59:45 +0000

    Fix CI

--- build/travis/build-vulkan-worker-linux.sh
+++ build/travis/build-vulkan-worker-linux.sh
@@ -27,7 +27,8 @@ mkdir -p vulkan-worker/build
 
 pushd vulkan-worker/build
 rm -rf ./*
-cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH="${HOME}/local/glfw" -E env CXXFLAGS="-Wno-missing-field-initializers"
+export CXXFLAGS="-Wno-missing-field-initializers"
+cmake ../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH="${HOME}/local/glfw"
 cmake --build . --config Debug -- -j$(nproc)
 popd
 
